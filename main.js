d197a75d4170cc7990d7664e33548b5760cd7b4c
HAUDRAUFHAUN <dietrich.poensgen@heg-portal.de>
2021-12-13 14:05:31 +0100

    update css & and js functionality

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 import "./style.css";
 alert("Willkommen auf unserer tollen Website!");
+
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.start();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        wuerg: "rgb(248 51 196)",
+        neon: "#39ff14",
+      },
+    },
   },
   plugins: [],
-}
+};
